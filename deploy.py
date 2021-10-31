12cd12466fd8aad3af8d161d04c01d2c8878dd99
jayakumar9 <93363487+jayakumar9@users.noreply.github.com>
2021-10-31 12:34:16 +0530

    Update deploy.py
    updated upto line82

--- deploy.py
+++ deploy.py
@@ -2,6 +2,9 @@ from solcx import compile_standard
 import json
 from web3 import web3
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 with open("./SimpleStorage.sol", "r") as file:
     simple_storage_file = file.read()
@@ -53,13 +56,31 @@ nonce=w3.eth.getTransactionCount(my_address)
 
 #1.Build a transaction
 #2.Sign a transaction
-#3.Send a transaction
+
 
 #1.Build a transaction as follows
 tranaction= SimpleStorage.constructor().buildTrnsaction({"chainId":chain_id,"from":my_address,"nonce":nonce})
 # print(transaction) to check for execution with command "python deploy.py"-result will be display in binary code format
 
 #create new file for python environment (.env)
+
+
+#2.Sign a transaction now
+signed_txn=w3.eth.account.sign_transaction(transaction,private_key-private_key)
+
+#3.Send this signed transaction for verification
+tx_hash=w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+tx_receipt=w3.eth.wait_for_transaction_receipt(tx_hash)
+# refer  1.solidity  vedio at 4:08:30
+
+# Working with the contract,you always need
+# Contract Address
+# Contract ABI
+simple_storage=w3.eth.contract(address=tx_receipt.contractAddress,abi=abi)
+# Call -> simulate making the call and getting a return value
+# Tranact -> Actually make a state change
+print(simple_storage.functions.retrive().call())
+
 
 
 
